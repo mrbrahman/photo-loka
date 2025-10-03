827223b0b311a8d61fc9f608fb6a51e3eb131dc3
Shreyas <shreyasls@yahoo.com>
2025-10-03 07:21:22 -0400

    Fixed nightly indexing scheduled time

--- app/jobs/nightly-indexing-job.mjs
+++ app/jobs/nightly-indexing-job.mjs
@@ -14,7 +14,7 @@ export function startNightlyIndexing() {
     return;
   }
   
-  addJob('nightly-indexing', '35 6 * * *', run);
+  addJob('nightly-indexing', '0 1 * * *', run);
 }
 
 export function stopNightlyIndexing() {
