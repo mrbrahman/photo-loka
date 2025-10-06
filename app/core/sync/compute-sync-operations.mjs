b639d2a5f7ca7cdd6128b36a9fe95a19cae675ba
mrbrahman <shreyasls@yahoo.com>
2025-10-06 19:42:57 -0400

    Core logic to determine effective sync operations

---
+++ app/core/sync/compute-sync-operations.mjs
@@ -0,0 +1,193 @@
+import * as path from 'path';
+
+let listenPaths = [
+  'listen1', 'listen2'
+];
+
+let collectionPaths = [
+  '/path/to/collection/'
+];
+
+let target = 'backup';
+
+// Scenario 1: simple one
+// let changes = [
+//   {action: 'create-dir', path1: null, path2: '/path/to/collection/dir1'},
+//   {action: 'move', path1: 'listen1/file11.jpg', path2: '/path/to/collection/dir1/file11.jpg'},
+//   {action: 'move', path1: 'listen1/file12.jpg', path2: '/path/to/collection/dir1/file12.jpg'},
+//   {action: 'move', path1: 'listen2/file13.jpg', path2: '/path/to/collection/dir1/file13.jpg'}
+// ];
+
+// Scenario 2: with rename of dir
+// let changes = [
+//   {action: 'create-dir', path1: null, path2: '/path/to/collection/dir1'},
+//   {action: 'move', path1: 'listen1/file11.jpg', path2: '/path/to/collection/dir1/file11.jpg'},
+//   {action: 'move', path1: 'listen1/file12.jpg', path2: '/path/to/collection/dir1/file12.jpg'},
+//   {action: 'move', path1: 'listen2/file13.jpg', path2: '/path/to/collection/dir1/file13.jpg'},
+  
+//   {action: 'move', path1: '/path/to/collection/dir1', path2: '/path/to/collection/dir2'},
+// ];
+
+// Scenario 3: dir1 already created in backup
+// let changes = [
+//   {action: 'move', path1: 'listen1/file11.jpg', path2: '/path/to/collection/dir1/file11.jpg'},
+//   {action: 'move', path1: 'listen1/file12.jpg', path2: '/path/to/collection/dir1/file12.jpg'},
+//   {action: 'move', path1: 'listen2/file13.jpg', path2: '/path/to/collection/dir1/file13.jpg'},
+  
+//   {action: 'move', path1: '/path/to/collection/dir1', path2: '/path/to/collection/dir2'},
+// ];
+
+// Scenario 4: new dir, file moved from listen path, and then moved to another album, original dir deleted
+// let changes = [
+//   {action: 'create-dir', path1: null, path2: '/path/to/collection/dir1'},
+//   {action: 'move', path1: 'listen1/file11.jpg', path2: '/path/to/collection/dir1/file11.jpg'},
+//   {action: 'move', path1: '/path/to/collection/dir1/file11.jpg', path2: '/path/to/collection/dir2/file11.jpg'},
+  
+//   {action: 'delete', path1: '/path/to/collection/dir1', path2: null},
+// ];
+
+let changes = [
+  {action: 'in-place', path1: null, path2: '/path/to/collection/dir1/file11.jpg'},
+  {action: 'move', path1: '/path/to/collection/dir1/file11.jpg', path2: '/path/to/collection/dir2/file11.jpg'},
+  
+  {action: 'delete', path1: '/path/to/collection/dir1', path2: null},
+];
+
+function getRelativePath(fullPath) {
+  for (let colPath of collectionPaths) {
+    if (fullPath.startsWith(colPath)) {
+      return fullPath.substring(colPath.length);
+    }
+  }
+  return null;
+}
+
+function getTargetPath(srcPath) {
+  for (let colPath of collectionPaths) {
+    if (srcPath.startsWith(colPath)) {
+      return path.join(
+        target, 
+        srcPath.substring(colPath.length)
+      );
+    }
+  }
+  return null;
+}
+
+function isListenPath(fullPath) {
+  for (let listenPath of listenPaths) {
+    if (fullPath.startsWith(listenPath)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function computeSyncOperations(changes) {
+  let effective = [];
+  let pathChanges = new Map();
+
+  // Given a dir path, check if any of its parent dirs have been moved
+  // If so, return the final effective dir
+  function effectiveDir(dirPath){
+    // recursively lookup path changes
+    if (pathChanges.has(dirPath)) {
+      return effectiveDir(pathChanges.get(dirPath));
+    }
+    return dirPath;
+  }
+
+  // Given an index and a path, check if there are further changes to this path
+  // If so, return the final effective path (or null if deleted)
+  // Mark all intermediate changes as 'skip', so they are not processed again
+
+  function effectivePath(idx, idxPath) {
+    for (let i=idx+1; i<changes.length; i++) {
+      let ch = changes[i];
+      if(ch.path1 === idxPath) {
+        if (ch.action === 'delete') {
+          // skip the current action, this is eventually deleted, and cannot be found
+          ch.skip = true;
+          return null;
+        }
+        else if (ch.action === 'move') {
+          // note down the change
+          pathChanges.set(idxPath, ch.path2);
+          // skip the current action, this is eventually moved, and cannot be found
+          ch.skip = true;
+          
+          // recursively check further changes
+          return effectivePath(i, ch.path2);
+        }
+      }
+    }
+    // no further changes found, return current path, but apply effective dir changes on files
+    return path.extname(idxPath) ? path.join(effectiveDir(path.dirname(idxPath)), path.basename(idxPath)) : idxPath;
+  }
+
+  for (let [idx, change] of changes.entries()) {
+    if (change.skip) continue;
+
+    if (change.action === 'create-dir') {
+      // first get effective path (in case of further rename dirs)
+      let currentPath = effectivePath(idx, change.path2);
+      if (currentPath){
+        effective.push({
+          'action': 'create-dir', 
+          path1: null, 
+          path2: getTargetPath(currentPath)
+        });
+      }
+    }
+
+    else if (change.action === 'in-place') {
+      let currentPath = effectivePath(idx, change.path2);
+      if (currentPath) {
+        effective.push({
+          'action': 'dir-and-copy', 
+          path1: currentPath, 
+          path2: getTargetPath(currentPath)
+        });
+      }
+    }
+    
+    else if (change.action === 'move') {
+      if (isListenPath(change.path1)) {
+        // file is moved from listen path to collection
+        let currentPath = effectivePath(idx, change.path2);
+        if (currentPath) {
+          effective.push({
+            'action': 'copy', 
+            path1: currentPath, 
+            path2: getTargetPath(currentPath)
+          });
+        }
+      }
+      else {
+        // file is moved within collection
+        let fromPath = getTargetPath(change.path1);
+        let toPath = getTargetPath(effectivePath(idx, change.path2));
+
+        if(toPath){
+          effective.push({
+            'action': 'move', 
+            path1: fromPath, 
+            path2: toPath
+          });
+        }
+      }
+    }
+
+    else if (change.action === 'delete') {
+      effective.push({
+        'action': 'delete', 
+        path1: getTargetPath(change.path1), 
+        path2: null
+      })
+    }
+  }
+  return effective;
+}
+
+let e = computeSyncOperations(changes);
+console.log(e);
